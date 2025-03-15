c657dbdcade651dafdbb0ea0d4a1d47307763855
FerreroRocket <jonnypollard100@gmail.com>
2025-03-15 16:12:23 +0000

    add post to create new round (not working)

--- api.py
+++ api.py
@@ -1,15 +1,24 @@
 import flask
 import database_interface
 from database_interface import Round, Session, select, engine
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route("/round/<id>", methods=["GET"])
-def home(id):
+def get_round(id):
     session = Session(engine)
     round = session.query(Round).get(id)
     return jsonify(round.to_dict())
+
+@app.route("/round", methods=["POST"])
+def add_round():
+    session = Session(engine)
+    data = request.get_json()
+    new_round = Round(data)
+    session.add(new_round)
+    session.commit()
+    return jsonify(new_round.to_dict()), 201 
 
 # Run the app with: python filename.py
 if __name__ == "__main__":
